7dc0a6588bc2059fbb922f0662c6c925cf96be46
kais sellami <kaissellami1@gmail.com>
2025-10-20 09:17:36 +0200

    tp4 avec des verifications a faire Q9 et Q6

--- tp4/README.md
+++ tp4/README.md
@@ -42,6 +42,10 @@ des espaces d'adressage.
 **Q3\* : Modifier le registre CR0 de sorte à activer la pagination dans `tp.c`.
   Que se passe-t-il ? Pourquoi ?**
 
+***reponse***
+En mettant le bit PG de cr0 à 1, on active la pagination 
+En executant qemu, on se retrouve avec une faute comme les entrees ne sont pas configurées : un mapping faux => page fault et au bout de 3 fautes, reset cest pour cela que l'emulateur restart <=> reset de la machine 
+
 **Q4\* : Un certain nombre de choses restent à configurer avant l'activation de
   la pagination. Comme pour le PGD, allouer également une PTB de type `
   (pte32_t*)` à l'adresse `0x601000`.**
@@ -55,14 +59,26 @@ des espaces d'adressage.
 * **Préparer au moins une entrée dans le PGD pour la PTB.**
 * **Préparer plusieurs entrées dans la PTB.**
 
+***reponse***
+Program Headers:
+  Type           Offset   VirtAddr   PhysAddr   FileSiz MemSiz  Flg Align
+  LOAD           0x000094 0x00300000 0x00300000 0x0000c 0x0000c RWE 0x4
+  LOAD           0x000000 0x00300010 0x00300010 0x00000 0x02000 RW  0x10
+  LOAD           0x0000b0 0x00302010 0x00302010 0x02a00 0x03630 RWE 0x20
+
 **Q6 : Une fois la pagination activée, essayer d'afficher le contenu d'une
   entrée de votre PTB. Que se passe-t-il ? Trouver la solution pour être
   capable de modifier les entrées de votre PTB une fois la pagination
   activée.**
+***reponse***
+page fault comme l'@ de ptb n'a pas de mapping => solution rajouter une page qui contient pdg et ptb
 
 **Q7\* : Avant d'activer la pagination, on souhaiterait faire en sorte que
   l'adresse virtuelle `0xc0000000` permette de modifier votre PGD après
   activation de la pagination. Comment le réaliser ?**
+***reponse***
+On veut accéder au PGD après activation de la pagination via l’adresse virtuelle 0xc0000000
+en mettant à l'indice 768 de pgd l'@ 0x00400000>>12 (la table qui contient le pgd) et puis de mettre dans l'entree 0 l'@ 0x600000>>12
 
 
 ## Quelques exercices supplémentaires de configuration spécifique
@@ -70,6 +86,13 @@ des espaces d'adressage.
 **Q8 : Faire en sorte que les adresses virtuelles `0x700000` et `0x7ff000`
   mappent l'adresse physique `0x2000`. Affichez la chaîne de caractères à ces
   adresses virtuelles.**
+***reponse***
+chaine a l'@ 0x700000 = /kernel.elf
+chaine a l'@ 0x7ff000 = /kernel.elf
 
 **Q9 : Effacer la première entrée du PGD. Que constatez-vous ? Expliquez
   pourquoi ?**
+***reponse***
+En remettant à 0 les champs de pgd[0] apres l'activation de la pagination, aucune caracteristique n'est visualisée. 
+Si on ne l'active pas dès le debut => page fault 
+//todo : voir ce qu'on cherche exactement en maz pgd[0]
--- tp4/tp.c
+++ tp4/tp.c
@@ -1,6 +1,123 @@
 /* GPLv2 (c) Airbus */
 #include <debug.h>
+#include <cr.h>
+#include <pagemem.h>
+#include <types.h>
 
 void tp() {
-	// TODO
+	uint32_t cr3 = get_cr3();
+	debug("addr CR3 = 0x%x\n", cr3); //addr CR3 = 0x0
+
+	//Q2
+	pde32_t* pgd = (pde32_t*)0x600000;
+	//set_cr3(pgd);
+	debug("addr CR3 = 0x%x\n", get_cr3());
+	//Q3
+	//set_cr0(get_cr0() | (0x1<<31));
+	//debug("cr0.PG = %d\n", 0x1 & get_cr0());
+
+	//Q4
+	pte32_t* ptb = (pte32_t*)0x601000;
+
+	
+	pgd[0].addr = (unsigned int)ptb>>12; // adresse sur les bits 31:12
+	pgd[0].p = 0x1; // page present
+	pgd[0].rw = 0x1;
+	debug("premiere entree de pgd : 0x%x \n", (unsigned int) pgd[0].addr);
+	set_cr3(pgd);
+
+	//Q5
+	//  Type           Offset   VirtAddr   PhysAddr   FileSiz MemSiz  Flg Align
+	//  LOAD           0x000094 0x00300000 0x00300000 0x0000c 0x0000c RWE 0x4
+	ptb[0x00300000>>12].addr = 0x00300000>>12;
+	ptb[0x00300000>>12].p = 1;
+	ptb[0x00300000>>12].rw = 1;
+	//  LOAD           0x000000 0x00300010 0x00300010 0x00000 0x02000 RW  0x10
+	ptb[0x00301000>>12].addr = 0x00301000>>12;
+	ptb[0x00301000>>12].p = 1;
+	ptb[0x00301000>>12].rw = 1;
+	
+	//  LOAD           0x0000b0 0x00302010 0x00302010 0x02a00 0x03630 RWE 0x20
+	ptb[0x00302010>>12].addr = 0x00302010>>12;
+	ptb[0x00302010>>12].p = 1;
+	ptb[0x00302010>>12].rw = 1;
+	
+	ptb[0x00303010>>12].addr = 0x00303010>>12;
+	ptb[0x00303010>>12].p = 1;
+	ptb[0x00303010>>12].rw = 1;
+
+	ptb[0x00304010>>12].addr = 0x00304010>>12;
+	ptb[0x00304010>>12].p = 1;
+	ptb[0x00304010>>12].rw = 1;
+
+	ptb[0x00305010>>12].addr = 0x00305010>>12;
+	ptb[0x00305010>>12].p = 1;
+	ptb[0x00305010>>12].rw = 1;
+
+	ptb[0x00306010>>12].addr = 0x00306010>>12;
+	ptb[0x00306010>>12].p = 1;
+	ptb[0x00306010>>12].rw = 1;
+
+		//Q6
+	//debug("afficher le contenu du premier PTE: @ = 0x%x\n", (unsigned int)pgd[0].addr); //error page fault
+	pte32_t* ptb_2 = (pte32_t*)0x00400000;
+	pgd[1].addr = (unsigned int)ptb_2>>12;
+	pgd[1].p = 1;
+	pgd[1].rw = 1;
+	
+	ptb_2[(0x600000>>12)&0x3FF].addr = 0x600000>>12;
+	ptb_2[(0x600000>>12)&0x3FF].p = 1;
+	ptb_2[(0x600000>>12)&0x3FF].rw = 1;
+	
+	ptb_2[(0x601000>>12)&0x3FF].addr = 0x601000>>12;
+	ptb_2[(0x601000>>12)&0x3FF].p = 1;
+	ptb_2[(0x601000>>12)&0x3FF].rw = 1;
+	//todo : voir pourquoi l'@ 0x601000 est elle accessible meme quand je ne fais pas son mapping mais je fais celui de 0x600000 
+	debug("afficher le contenu du premier PTE: @ = 0x%x\n", (unsigned int)pgd[0].addr); //error page fault
+	//Fin Q6	
+
+	//Q7
+	pgd[768].addr = (unsigned int)ptb_2>>12;
+	pgd[768].p = 1;
+	pgd[768].rw = 1;
+
+	ptb_2[0].addr = (unsigned int) pgd>>12;
+	ptb_2[0].p = 1;
+	ptb_2[0].rw = 1;
+	
+	/*
+	set_cr0(get_cr0() | (0x1<<31));
+	debug("cr0.PG = %d\n", 0x1 & get_cr0());
+
+	debug("contenu de pgd addr = 0x%x\n", pgd[0].addr);
+	pde32_t* var = (pde32_t*) 0xc0000000;
+	debug("contenu de l'@ 0xc0000000 = 0x%x\n", var->addr);
+	*/
+	//Fin Q7
+
+	//Q8
+	//0x700000 => 1 (pgd)| 768 (ptb)| 0 (offset) 
+	ptb_2[768].addr = 0x2000 >> 12;
+	ptb_2[768].p = 1;
+	ptb_2[768].rw = 1;
+
+	//0x7ff000 => 1 (pgd)| 1023 (ptb)| 0 (offset) 
+	ptb_2[1023].addr = 0x2000>>12;
+	ptb_2[1023].p = 1;
+	ptb_2[1023].rw = 1;
+
+	set_cr0(get_cr0() | (0x1<<31));
+	debug("cr0.PG = %d\n", 0x1 & get_cr0());
+
+	char* ch1 = (char*) 0x700000;
+	char* ch2 = (char*) 0x7ff000;
+	debug("chaine a l'@ 0x700000 = %s\n", ch1);
+	debug("chaine a l'@ 0x7ff000 = %s\n", ch2);
+	//Fin Q8
+
+	//Q9
+	//pgd[0].p = 0;
+	//pgd[0].addr = 0x0;
+	//pgd[0].rw = 0;
+	
 }
